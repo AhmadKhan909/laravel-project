b019b072ea91c078e68453e04be35917cc4fc666
Tabish Ahmad <tabishahmad804@gmail.com>
2023-03-21 17:51:55 +0500

    header, footer and login added

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center my-5 custom-login">
+            <div class="col-sm-4">
+                <form class="px-5 py-5" style="border: 1px solid #000;">
+                    <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1">
+                    <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
